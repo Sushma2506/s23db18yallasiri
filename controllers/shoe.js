6bd4f3f3e4aeb460ad2b5f6cc432645a1d84b865
Sushma2506 <122947550+Sushma2506@users.noreply.github.com>
2023-04-14 13:32:45 -0500

    commit

--- controllers/shoe.js
+++ controllers/shoe.js
@@ -60,3 +60,48 @@ exports.shoe_create_post = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
    };
+   // for a specific Costume.
+exports.shoe_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await shoe.findById( req.params.id)
+    //console.log(req.params.id)
+    //console.log(result)
+    res.send(String(result))
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found}`);
+    }
+    };
+// Handle Shoe update form on PUT.
+exports.shoe_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body
+   ${JSON.stringify(req.body)}`)
+    try {
+    let toUpdate = await shoe.findById( req.params.id)
+    // Do updates of properties
+    if(req.body.shoeSize)
+    toUpdate.shoeSize = req.body.shoeSize;
+    if(req.body.shoeType) toUpdate.shoeType = req.body.shoeType;
+    if(req.body.shoeBrand) toUpdate.shoeBrand = req.body.shoeBrand;
+    let result = await toUpdate.save();
+    console.log("Sucess " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": ${err}: Update for id ${req.params.id}
+   failed`);
+    }
+   };  
+// Handle shoe delete on DELETE.
+exports.shoe_delete = async function(req, res) {
+    console.log("delete " + req.params.id)
+    try {
+    result = await Costume.findByIdAndDelete( req.params.id)
+    console.log("Removed " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": Error deleting ${err}}`);
+    }
+   };
