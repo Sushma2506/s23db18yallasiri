832b76ef46bffdc598f8caa2510c8c738f8b74cb
Sushma2506 <122947550+Sushma2506@users.noreply.github.com>
2023-04-17 13:21:32 -0500

    commit

--- controllers/shoe.js
+++ controllers/shoe.js
@@ -93,15 +93,4 @@ exports.shoe_update_put = async function(req, res) {
    failed`);
     }
    };  
-// Handle shoe delete on DELETE.
-exports.shoe_delete = async function(req, res) {
-    console.log("delete " + req.params.id)
-    try {
-    result = await Costume.findByIdAndDelete( req.params.id)
-    console.log("Removed " + result)
-    res.send(result)
-    } catch (err) {
-    res.status(500)
-    res.send(`{"error": Error deleting ${err}}`);
-    }
-   };
+
--- routes/resource.js
+++ routes/resource.js
@@ -9,8 +9,8 @@ router.get('/', api_controller.api);
 /// SHOE ROUTES ///
 //POST request for creating a Shoe.
 router.post('/shoes', shoe_controller.shoe_create_post);
-// DELETE request to delete Shoe.
-router.delete('/shoes/:id', shoe_controller.shoe_delete);
+// // DELETE request to delete Shoe.
+// router.delete('/shoes/:id', shoe_controller.shoe_delete);
 // PUT request to update Shoe.
 router.put('/shoes/:id', shoe_controller.shoe_update_put);
 // GET request for one Shoe.
